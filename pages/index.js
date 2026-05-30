c5d0a0ee7610f0976e222a00081cf9b64648ddc2
Štěpán Janás <stepajanas@gmail.com>
2026-05-30 21:07:53 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
+import Calendar from "../components/Calendar";
+import ReservationForm from "../components/ReservationForm";
+
 export default function Home() {
   return (
     <div>
       <h1>Zvučení Blatnice pod sv. Ant.</h1>
       <p>Kalendář akcí a rezervací</p>
+      <Calendar />
+      <ReservationForm />
     </div>
   );
 }
